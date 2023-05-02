6c17eea2f1a8a2bc6284ee5c8f919ce315fed838
Akash Patel <akashpatelsa@gmail.com>
2023-05-01 20:43:28 -0500

    Timer

--- main.py
+++ main.py
@@ -19,14 +19,32 @@ def main():
     tft.fill(gc9a01.BLACK)
     
     def show_text(text):
-        tft.draw(romand, text, 15, 115, gc9a01.WHITE)
+        tft.draw(romand, text, 120, 115, gc9a01.WHITE)
+        
+    def show_m(text):
+        tft.draw(romand, text, 65,115,gc9a01.WHITE)
     
     def show_jpg(jpg):
         tft.jpg(jpg, 30,0, gc9a01.FAST)
+        
+    def seconds():
+        pass
+    min = 10
 
     while True:
-        show_text("Hello World")
-        show_jpg("11d.jpg")
+        # Dynamicall display i to the screen
+        for i in range(0, 61):
+            if i == 60:
+                min += 1
+            if min != 0:
+                show_m(str(min))
+                
+            show_text(str(i))
+            time.sleep(.1)
+            tft.fill(gc9a01.BLACK)
+            
+                
+        
         '''
         tft.jpg("11d.jpg", 30, 20, gc9a01.FAST)
         tft.draw(romand, "75 F", 130, 70, gc9a01.WHITE) # x, y coords
@@ -38,3 +56,5 @@ def main():
         '''
 
 main()
+
+
