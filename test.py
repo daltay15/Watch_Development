88c1e85af452e6535d84a414a6e472340bf12e05
Demir Altay <77849115+daltay15@users.noreply.github.com>
2023-05-02 11:05:37 -0500

    Working time and date

--- test.py
+++ test.py
@@ -21,23 +21,37 @@ def main():
 
         
     def show_time():
-        while True:
-            current_time = time.localtime()
-            hour = str(current_time[3])
-            minute = str(current_time[4])
-            second = str(current_time[5])
-            if len(hour) == 1:
-                hour = '0' + hour
-            if len(minute) == 1:
-                minute = '0' + minute
-            if len(second) == 1:
-                second = '0' + second
-            time_string = hour + ':' + minute + ':' + second
-            tft.draw(romand, time_string, 45, 120, gc9a01.WHITE)
-            time.sleep(1)
-            tft.fill(gc9a01.BLACK)
+        current_time = time.localtime()
+        hour = str(current_time[3])
+        minute = str(current_time[4])
+        second = str(current_time[5])
+        if len(hour) == 1:
+            hour = '0' + hour
+        if len(minute) == 1:
+            minute = '0' + minute
+        if len(second) == 1:
+            second = '0' + second
+        time_string = hour + ':' + minute + ':' + second
+        tft.draw(romand, time_string, 45, 120, gc9a01.WHITE)
+        
+    def show_date():
+        current_date = time.localtime()
+        month = str(current_date[1])
+        day = str(current_date[2])
+        year = str(current_date[0])
+        if len(month) == 1:
+            month = '0' + month
+        if len(day) == 1:
+            day = '0' + day
+        date_string = month + '/' + day + '/' + year
+        tft.draw(romand, date_string, 15, 70, gc9a01.WHITE)
+        
+    while True:
+        show_time()
+        show_date()
+        time.sleep(1)
+        tft.fill(gc9a01.BLACK)
     
-    show_time()
-
 main()
+
 
